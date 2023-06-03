1586da91c01afeb37e5657ab4adf063d479dd33a
Lucas Alves <lucas.soares008@edu.pucrs.br>
2023-06-03 20:18:15 -0300

    Coloquei o ID do porto na classe Nodo pra gente poder ordenar os portos pra busca. Só portos têm o ID, que equivale ao número deles

--- main.java
+++ main.java
@@ -18,25 +18,25 @@ public class main {
         // Realize uma busca em largura para cada porto e calcule o combustível necessário
                 // Realize uma busca em largura para cada porto e calcule o combustível necessário
                 int combustivelNecessario = 0;
-                Nodo ultimoNodo = portos.get(0);
+                Nodo ultimoPorto = portos.get(0);
         
                 for (int i = 1; i < portos.size(); i++) {
                     Nodo portoAtual = portos.get(i);
         
                     // Verifique se o porto atual existe no grafo
                     if (grafo.getNodos().contains(portoAtual)) {
-                        bfs.bfs(ultimoNodo);
+                        bfs.bfs(ultimoPorto);
         
                         // Supondo que cada movimento consome uma unidade de combustível,
                         // o combustível necessário é igual à distância entre os portos
                         combustivelNecessario += bfs.distancia(portoAtual);
-                        ultimoNodo = portoAtual;
+                        ultimoPorto = portoAtual;
                     }
                 }
         
                 // Volte para o primeiro porto
                 if (grafo.getNodos().contains(portos.get(0))) {
-                    bfs.bfs(ultimoNodo);
+                    bfs.bfs(ultimoPorto);
                     combustivelNecessario += bfs.distancia(portos.get(0));
                 }
         
